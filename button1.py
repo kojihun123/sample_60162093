40dca9435ad96741c96727f542184db75b6e1c8f
kojihun <rhehddl97@gamil.com>
2020-11-02 16:35:24 +0900

    e

--- button1.py
+++ button1.py
@@ -7,18 +7,14 @@ b2 = Button(window, text="60162093")
 b3 = Button(window, text="hello")
 b4 = Button(window, text="bye")
 b5 = Button(window, text="dsfsd")
-<<<<<<< HEAD
-=======
 b6 = Button(window, text="abcd")
->>>>>>> new1
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 b3.grid(row=1, column=0)
 b4.grid(row=2, column=0)
 b5.grid(row=2, column=1)
-<<<<<<< HEAD
-=======
 b6.grid(row=3, column=0)
->>>>>>> new1
+
+print("hi")
 
 window.mainloop()
